ccdf7f1870f7bc95bc795a8266bab44175180347
Joshua-Dunne <c00241588@itcarlow.ie>
2021-11-01 11:54:45 +0000

    Feature One Complete - Transition Types

---
+++ Packages/ie.itcarlow.screentransitions/Runtime/ScreenTransition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ScreenTransition : MonoBehaviour
+{
+    public Camera cam;
+
+    public enum transitionTypes { HORIZONTAL, VERTICAL };
+    public transitionTypes type = transitionTypes.HORIZONTAL;
+    public Vector2 transitionPoint = Vector2.zero;
+    private bool transitioning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // this component must be attached to a camera
+        cam = this.gameObject.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Packages/ie.itcarlow.screentransitions/Runtime/ScreenTransitionEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(ScreenTransition))]
+public class ScreenTransitionEditor : Editor
+{
+    protected virtual void OnSceneGUI()
+    {
+        ScreenTransition customClass = (ScreenTransition)target;
+
+        // Safety against selecting a null GameObject
+        if (customClass == null)
+        {
+            return;
+        }
+
+        Handles.color = Color.yellow;
+        Handles.DrawLine(customClass.transform.position, customClass.transitionPoint);
+        Vector2 newPos = Handles.PositionHandle(customClass.transitionPoint, Quaternion.identity);
+
+        if (customClass.type == ScreenTransition.transitionTypes.HORIZONTAL)
+        {
+            customClass.transitionPoint.x = newPos.x;
+            customClass.transitionPoint.y = customClass.transform.position.y;
+        }
+        else
+        {
+            customClass.transitionPoint.x = customClass.transform.position.x;
+            customClass.transitionPoint.y = newPos.y;
+        }
+            
+    }
+}
