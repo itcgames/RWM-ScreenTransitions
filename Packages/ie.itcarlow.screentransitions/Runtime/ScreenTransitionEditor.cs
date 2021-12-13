22741aa49cd2f94b58680043891b28d1253ca8b6
Joshua Dunne <44901016+Joshua-Dunne@users.noreply.github.com>
2021-12-13 09:14:38 +0000

    AZ-4 Transition Points (#1)
    * All points have handles in editor
    
    * Lines are now drawn between all points
    
    * Re-added unity editor check
    
    * Added Tests for Adding and Removing Points

--- Packages/ie.itcarlow.screentransitions/Runtime/ScreenTransitionEditor.cs
+++ Packages/ie.itcarlow.screentransitions/Runtime/ScreenTransitionEditor.cs
@@ -9,31 +9,90 @@ using UnityEngine;
 [CustomEditor(typeof(ScreenTransition))]
 public class ScreenTransitionEditor : Editor
 {
+    private ScreenTransition sT;
+    private TransitionPoint currPoint = new TransitionPoint();
+	private Camera mainCam;
+
+    private void OnEnable()
+	{
+		// Method 1
+		mainCam = Camera.main;
+		sT = mainCam.GetComponent<ScreenTransition>();
+        currPoint.transitionPoint = mainCam.transform.position + new Vector3(1, 1, 0);
+	}
+
     protected virtual void OnSceneGUI()
     {
-        ScreenTransition customClass = (ScreenTransition)target;
-
         // Safety against selecting a null GameObject
-        if (customClass == null)
+        if (sT == null)
         {
             return;
         }
 
-        Handles.color = Color.yellow;
-        Handles.DrawLine(customClass.transform.position, customClass.transitionPoint);
-        Vector2 newPos = Handles.PositionHandle(customClass.transitionPoint, Quaternion.identity);
-
-        if (customClass.type == ScreenTransition.transitionTypes.HORIZONTAL)
+        // draw Handles for all points
+        if(sT.transitionPoints.Count > 0)
         {
-            customClass.transitionPoint.x = newPos.x;
-            customClass.transitionPoint.y = customClass.transform.position.y;
+            foreach(TransitionPoint point in sT.transitionPoints)
+            {
+                Handles.color = Color.yellow;
+                Vector2 pos = Handles.PositionHandle(point.transitionPoint, Quaternion.identity);
+                point.transitionPoint = pos;
+            }
+        }
+
+        // draw lines between all points in the vector
+        if (sT.transitionPoints.Count > 0)
+        {
+            Handles.DrawLine(mainCam.transform.position, sT.transitionPoints[0].transitionPoint);
+
+            for (int i = 0; i < sT.transitionPoints.Count; i++)
+            {
+                if (i + 1 >= sT.transitionPoints.Count)
+                {
+                    break;
+                }
+
+                Handles.DrawLine(sT.transitionPoints[i].transitionPoint, sT.transitionPoints[i + 1].transitionPoint);
+            }
+
+        }
+
+        // now draw a line from the last point added to the current point being edited
+        Handles.color = new Color(0, 1, 0, 1);
+
+        if (sT.transitionPoints.Count > 0)
+        {
+            Handles.DrawLine(sT.transitionPoints[sT.transitionPoints.Count - 1].transitionPoint, currPoint.transitionPoint);
         }
         else
         {
-            customClass.transitionPoint.x = customClass.transform.position.x;
-            customClass.transitionPoint.y = newPos.y;
-        }
-            
+            Handles.DrawLine(mainCam.transform.position, currPoint.transitionPoint);
+        } 
+
+        // handle point used to add new transition points to the list
+        Vector2 newPos = Handles.PositionHandle(currPoint.transitionPoint, Quaternion.identity);
+        currPoint.transitionPoint = newPos;
+        Handles.Label(newPos, "Current Point");
     }
+
+    public override void OnInspectorGUI()
+	{
+		if (GUILayout.Button("Add Point"))
+		{
+            currPoint.type = sT.type;
+            Vector2 vec = currPoint.transitionPoint;
+            sT.AddPoint(currPoint);
+            currPoint = new TransitionPoint();
+            currPoint.transitionPoint = vec;
+        }
+
+        if (GUILayout.Button("Remove Last Point"))
+        {
+            sT.RemoveLastPoint();
+        }
+
+        // Draw default inspector after button...
+        base.OnInspectorGUI();
+	}
 }
 #endif
